79c2d2e25b38338b6cee19e294a85d07528864e1
Tarek Ahmed <tarekelfanty@gmail.com>
2026-02-22 12:05:06 +0200

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,20 +35,18 @@ export default function Home() {
 
         {/* CONTENT AREA */}
         <div className="flex-1 flex flex-col">
-          {/* HERO */}
-          {/* HERO */}
-<div className="mt-[14px] sm:mt-[24px] relative w-full bg-black overflow-hidden h-[38svh] sm:h-[46svh]">
+        <div className="mt-[14px] sm:mt-[24px] relative w-full bg-black overflow-hidden h-[38svh] sm:h-[46svh]">
   <div
     className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
                rounded-full overflow-hidden
-               w-[72vw] h-[72vw] max-w-[360px] max-h-[360px]
+               w-[78vw] h-[78vw] max-w-[380px] max-h-[380px]
                sm:w-[420px] sm:h-[420px]"
   >
     <Image
       src="/logos/hero.png"
       alt="Hero"
       fill
-      className="object-contain sm:object-cover"
+      className="object-cover"
       priority
     />
   </div>
