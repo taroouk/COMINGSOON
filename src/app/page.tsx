f88c1bcb923479aba2dc0da84c6660979419e1eb
Tarek Ahmed <tarekelfanty@gmail.com>
2026-02-22 12:50:12 +0200

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
     className="
       absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
       rounded-full overflow-hidden
-      w-[150vw] h-[150vw] max-w-[150px] max-h-[150px]
+      w-[150vw] h-[150vw] max-w-[380px] max-h-[380px]
       sm:w-[420px] sm:h-[420px]
     "
   >
