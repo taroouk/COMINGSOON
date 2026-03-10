98a67e4b11661bdb55aae4aaeed009bf211fb219
Tarek Ahmed <tarekelfanty@gmail.com>
2026-03-10 02:08:50 +0200

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() {
 
     {/* Instagram */}
     <a
-      href="https://instagram.com"
+      href="https://www.instagram.com/toogoodformerch"
       target="_blank"
       rel="noopener noreferrer"
       className="border border-[#bdbdbd] h-[56px] sm:h-[64px] grid grid-cols-[56px_1fr] sm:grid-cols-[64px_1fr] items-center"
@@ -101,7 +101,7 @@ export default function Home() {
 
     {/* TikTok */}
     <a
-      href="https://tiktok.com"
+      href="www.tiktok.com/@toogoodformerch"
       target="_blank"
       rel="noopener noreferrer"
       className="border border-[#bdbdbd] h-[56px] sm:h-[64px] grid grid-cols-[56px_1fr] sm:grid-cols-[64px_1fr] items-center"
